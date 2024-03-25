c37bdbbebf4c3528552d9ff7eecf56ffbf2a4f0b
Aidarkg <adijamgyrchiev@gmail.com>
2024-03-26 03:10:03 +0600

    third commit

--- flat/settings.py
+++ flat/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'aidarazh.pythonanywhere.com']
 
 
 # Application definition
